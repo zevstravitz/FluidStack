e3db4a9302621a385ec0c443a9d0971bf1f0f11b
Zev Stravitz <zevstravitz2021@u.northwestern.edu>
2018-06-09 21:26:44 -0500

    before adding the grid...updated buttons and such

--- addtolist.js
+++ addtolist.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
   $("#add").click(function() {
     var newListItem = $("#addtolist").val();
     var quantityNum = $("#quantity").val();
-    if ((newListItem.length > 0) && (quantityNum > 0) && (quantityNum < 60)) {
+    if ((newListItem.length > 0) && (quantityNum > 0) && (quantityNum <= 12)) {
       for (var i = 0; i < quantityNum; i++) {
-        $("#timestack2").append("<li>" + newListItem + "</li>");
+        $("#timestack1").append("<li>" + newListItem + "</li>");
         $("#addtolist").val('');
       }}
   });
